fc0c2c493528c1d9988d530e8863c63cc8637051
mihai.gomoi <mihai.gomoi@softvision.ro>
2015-07-15 13:15:32 +0300

    Fixed env port for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
     Hide internal paths for the outside world
 */
 app.use('/distribution', express.static(__dirname + '/node_modules/'));
-app.listen(process.env.HTTP_PORT || 1337, function(err) {
+app.listen(process.env.PORT || 1337, function(err) {
 	if (err) {
 		console.error(err);
 		process.exit(1);

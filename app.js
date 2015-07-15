7f8e73c4101ce9d2e97e2fd9bf092b52da7a6ae3
mihai.gomoi <mihai.gomoi@softvision.ro>
2015-07-15 14:42:36 +0300

    Modifications to Procfile

--- app.js
+++ app.js
@@ -3,11 +3,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-/*
-    Hide internal paths for the outside world
-*/
-//app.use('/node_modules', express.static(__dirname + '/node_modules/'));
-
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
 app.listen(process.env.PORT || 1337, function(err) {
 	if (err) {
 		console.error(err);

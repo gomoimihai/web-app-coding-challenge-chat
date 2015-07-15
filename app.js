b9ab69b4d48d59c49e9765cdb21e254c91744978
mihai.gomoi <mihai.gomoi@softvision.ro>
2015-07-15 15:00:57 +0300

    Modification for heroku deploy

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
-app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.use('/scripts', express.static(__dirname + '/node_modules'));
+
 app.listen(process.env.PORT || 1337, function(err) {
 	if (err) {
 		console.error(err);
